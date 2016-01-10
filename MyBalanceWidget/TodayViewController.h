8a17fbbf20356b1384c280b1e31f2b4ec67e2ff6
Tommy Sparber <tommy@sparber.net>
2016-01-10 11:53:05 +1100

    Create WidgetTarget

---
+++ MyBalanceWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  MyBalanceWidget
+//
+//  Created by Tommy Sparber on 10/01/2016.
+//  Copyright © 2016 Tommy Sparber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
